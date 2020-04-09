d7269394e30b2bff79b9af0fb554120bd34f7240
Phi Nam <phinamhd1992@gmail.com>
2020-04-09 13:59:10 +0700

    create deleteStudentById and updateStudent method

--- src/main/java/com/vsii/schedulingtask/contronller/StudentController.java
+++ src/main/java/com/vsii/schedulingtask/contronller/StudentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
@@ -38,6 +39,32 @@ public class StudentController {
         System.out.println("Date: " + date);
         studentService.save(student);
         return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateStudent(@RequestBody Student student, @PathVariable Long id){
+        Optional<Student> student1 = studentService.findById(id);
+        if (!student1.isPresent()){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        Date date = new Date();
+        student1.get().setName(student.getName());
+        student1.get().setAge(student.getAge());
+        student1.get().setAddress(student.getAddress());
+        student1.get().setCreateTime(date);
+
+        studentService.save(student1.get());
+        return new ResponseEntity(student1, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteStudentById(@PathVariable Long id){
+        Optional<Student> student = studentService.findById(id);
+        if (!student.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        studentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Scheduled(fixedRate = 600000)// 5 phút
